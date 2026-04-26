ec21abd12a7f0913a466b13a3e7faf3acc7a07ba
calmroshaan <calmroshaan@gmail.com>
2026-04-27 04:30:29 +0500

    Config: Hardcoded custom domains for production

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+
+ALLOWED_HOSTS = ['www.flashbite.store', 'flashbite.store', 'flashbite.pythonanywhere.com', 'localhost', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://www.flashbite.store', 'https://flashbite.store', 'https://flashbite.pythonanywhere.com']
 
 # Application definition
 INSTALLED_APPS = [
